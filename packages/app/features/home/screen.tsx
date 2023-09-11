4429cbffae6155828dd029f2658714cfcbd6f81b
Shvepsss <nastia-shviets@mail.ru>
2023-09-11 14:54:01 +0400

    chore: Typography example

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,12 +1,29 @@
 import { Text, useSx, View, H1, P, Row, A } from 'dripsy'
 import { TextLink } from 'solito/link'
 import { MotiLink } from 'solito/moti'
-
-import { Button } from 'app/shared/components/ui'
-import { useNavigation } from 'app/shared/hooks'
+import {
+  useDynamicStyles,
+  getDynamicStylesInput,
+  useNavigation,
+} from 'app/shared/hooks'
+import { Button, Typography } from 'app/shared/components/ui'
 import { APP_URL } from 'app/shared/constants'
 
+const dynamicStylesInput = getDynamicStylesInput((theme) => {
+  return {
+    wrapper: {
+      backgroundColor: theme.colors.error,
+    },
+
+    button: {
+      height: 100,
+      width: 100,
+    },
+  }
+})
+
 export function HomeScreen() {
+  const dynamicStyles = useDynamicStyles(dynamicStylesInput)
   const sx = useSx()
   const navigation = useNavigation()
   const redirectToUser = (userId: string) => {
@@ -16,7 +33,23 @@ export function HomeScreen() {
   return (
     <View
       sx={{ flex: 1, justifyContent: 'center', alignItems: 'center', p: 16 }}
+      style={dynamicStyles.wrapper}
     >
+      <Typography color="primary" variant="bodySmall">
+        primary
+      </Typography>
+      <Typography color="background" variant="bodyMedium">
+        background
+      </Typography>
+      <Typography color="surface" variant="headlineLarge">
+        surface
+      </Typography>
+      <Typography color="surfaceDisabled" variant="bodyMedium">
+        surfaceDisabled
+      </Typography>
+      <Typography color="secondary" variant="bodyMedium">
+        secondary
+      </Typography>
       <H1 sx={{ fontWeight: '800' }}>Welcome to Solito.</H1>
       <View sx={{ maxWidth: 600 }}>
         <P sx={{ textAlign: 'center' }}>
@@ -50,8 +83,17 @@ export function HomeScreen() {
         >
           Regular Link
         </TextLink>
-        <Button onPress={() => redirectToUser('Nsa')}>
-          <P>Regular Link Test</P>
+        <Button
+          onPress={() => redirectToUser('Nsa')}
+          style={[
+            // dynamicStyles.button,
+            dynamicStyles.wrapper,
+            { backgroundColor: 'white' },
+          ]}
+        >
+          <Typography color="primary" variant="headlineLarge">
+            Regular Link Test
+          </Typography>
         </Button>
         <View sx={{ width: 32 }} />
         <MotiLink

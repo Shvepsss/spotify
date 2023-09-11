5f2f368373f0dee1856b809a568fc7bf150dea24
Shvepsss <nastia-shviets@mail.ru>
2023-09-11 14:52:07 +0400

    feat: useDynamicStyles

---
+++ packages/app/shared/hooks/useDynamicStyles.ts
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { StyleSheet, ViewStyle, TextStyle, ImageStyle } from 'react-native'
+import { useTheme, MD3Theme } from 'react-native-paper'
+
+type Styles = ViewStyle | TextStyle | ImageStyle
+
+type NamedStyles<T> = { [P in keyof T]: Styles }
+
+type DynamicStylesInput = Record<string, Styles>
+
+export const getDynamicStylesInput = <T extends DynamicStylesInput>(
+  getStyles: (theme: MD3Theme) => T
+) => {
+  return getStyles
+}
+
+export const useDynamicStyles = <T extends NamedStyles<T>>(
+  getStyles: (theme: MD3Theme) => T
+) => {
+  const theme = useTheme()
+
+  const memoedStyles = React.useMemo(() => {
+    const stylesProps = getStyles(theme)
+
+    return StyleSheet.create(stylesProps)
+  }, [getStyles, theme])
+
+  return memoedStyles
+}

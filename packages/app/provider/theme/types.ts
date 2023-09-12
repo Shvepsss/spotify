151717b0c7726fc5d6e9a96c0327ebb7bf1e3fa2
Shvepsss <nastia-shviets@mail.ru>
2023-09-12 15:49:21 +0400

    chore: cleanup

--- packages/app/provider/theme/types.ts
+++ packages/app/provider/theme/types.ts
@@ -1,3 +1,5 @@
+import { MD3Theme as BaseMD3Theme, MD3TypescaleKey } from 'react-native-paper'
+
 export type TypographyVariants = 'headlineLarge' | 'bodyMedium' | 'bodySmall'
 
 export type MD3Colors =
@@ -6,3 +8,12 @@ export type MD3Colors =
   | 'surface'
   | 'surfaceDisabled'
   | 'secondary'
+
+export type MD3Theme = Omit<BaseMD3Theme, 'colors' | 'fonts'> & {
+  colors: {
+    [colorName in MD3Colors]: string
+  }
+  fonts: {
+    [variant in TypographyVariants]: BaseMD3Theme['fonts'][MD3TypescaleKey]
+  }
+}
--- packages/app/shared/components/ui/IconLocal/IconLocal.tsx
+++ packages/app/shared/components/ui/IconLocal/IconLocal.tsx
@@ -1,20 +1,20 @@
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import { MD3Theme, useTheme } from 'react-native-paper'
+import { useTheme } from 'app/shared/hooks/useTheme'
 import { ICON_NAMES } from 'app/shared/constants'
+import { MD3Colors } from 'app/provider/theme'
 
 type IconLocalProps = {
   iconName: keyof typeof ICON_NAMES
   size: number
-  color?: 'primary' | 'background' | 'surface' | 'surfaceDisabled' | 'secondary'
+  color?: MD3Colors
 }
 export const IconLocal = ({ iconName, size, color }: IconLocalProps) => {
-  const { colors } = useTheme()
-
+  const theme = useTheme()
   return (
     <Icon
       size={size}
       name={ICON_NAMES[iconName]}
-      color={color ? colors[color] : undefined}
+      color={color ? theme.colors[color] : undefined}
     />
   )
 }

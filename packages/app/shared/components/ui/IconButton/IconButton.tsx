9c832168549546764a5c0922f8d7199dead017ec
Shvepsss <nastia-shviets@mail.ru>
2023-09-20 15:55:39 +0400

    feat: icon button

---
+++ packages/app/shared/components/ui/IconButton/IconButton.tsx
@@ -0,0 +1,20 @@
+import { Pressable } from 'react-native'
+import { IconLocal, IconLocalProps } from '../IconLocal'
+import { ViewProps } from '../View'
+type IconButtonProps = Pick<ViewProps, 'style'> &
+  IconLocalProps & {
+    onPress: () => void
+  }
+export const IconButton = ({
+  iconName,
+  size,
+  color,
+  onPress,
+  style,
+}: IconButtonProps) => {
+  return (
+    <Pressable onPress={onPress} style={[style]}>
+      <IconLocal iconName={iconName} size={size} color={color} />
+    </Pressable>
+  )
+}
